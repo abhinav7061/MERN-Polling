73d8630d3cc83fc0fe41c00497680ab017b3676b
Abhinav Kumar <abhinavkumar93043@example.com>
2023-11-04 19:41:06 +0530

    added cors

--- api/app.js
+++ api/app.js
@@ -3,16 +3,18 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const app = express();
-const ErrorHandling=require("./middlewares/error")
+const ErrorHandling = require("./middlewares/error")
 
 dotenv.config({ path: "config/config.env" });
 app.use(express.json()); //middleware to understand json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+const cors = require('cors');
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
 
 // //importing routes
 const user = require("./Router/userRoutes");
-const poll =require("./Router/pollRoutes");
+const poll = require("./Router/pollRoutes");
 const vote = require("./Router/voteRoutes");
 
 // //using routes
