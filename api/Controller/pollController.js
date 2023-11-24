b97215596313d2a36927d34f2119ba5968f2768b
Abhinav Kumar <abhinavkumar93043@example.com>
2023-11-24 07:27:59 +0530

    searching and sorting poll

--- api/Controller/pollController.js
+++ api/Controller/pollController.js
@@ -1,178 +1,230 @@
-const {sendErrorResponse} =require("../middlewares/erroHandle")
-const Poll =require("../Models/PollSchema");
+const { sendErrorResponse } = require("../middlewares/erroHandle")
+const Poll = require("../Models/PollSchema");
 const User = require("../Models/UserSchema");
 
-exports.createPoll=async(req,res)=>{
+exports.createPoll = async (req, res) => {
     try {
-        const {title,description,options}= req.body;
-        if(!title){
-            return sendErrorResponse(res,401,"Title is required");
+        const { title, description, options } = req.body;
+        if (!title) {
+            return sendErrorResponse(res, 401, "Title is required");
         }
-        if(!description){
-            return sendErrorResponse(res,401,"description is required");
+        if (!description) {
+            return sendErrorResponse(res, 401, "description is required");
         }
-        if(!options || !Array.isArray(options)){
-            return sendErrorResponse(res,401,"options is required");
+        if (!options || !Array.isArray(options)) {
+            return sendErrorResponse(res, 401, "options is required");
         }
-        if(options.length<2){
-            return sendErrorResponse(res,401,"At least 2 subjects is required");
+        if (options.length < 2) {
+            return sendErrorResponse(res, 401, "At least 2 subjects is required");
         }
-        const poll=await Poll.create({title,description,options})
-        res.status(200).json({
-            success:true,
-            message:"Poll created Successfully",
-            poll
-        })
-    } catch (error) {
-        return sendErrorResponse(res,500,error.message)
-    }
-}
-
-exports.updatePoll = async (req,res) => {
-    try {
-        const fields=["title","description","options","startDate","endDate","tags"]
-        const update={}
-        fields.forEach((field)=>{
-            if(req.body[field] !== undefined){
-                if(field==='startDate' || field === 'endDate'){
-                    update[field]=new Date(req.body.field)
-                }
-                else{
-                    update[field]=req.body[field]
-                }
-            }
-        })
-        const poll = await Poll.findByIdAndUpdate(req.params.id,update,{new:true})
-        if(!poll){
-            return sendErrorResponse(res,404,"Poll not found")
-        }
-
-        res.status(200).json({
-            success:true,
-            message:"Poll updated successfully",
-            poll
-        })
-    } catch (error) {
-        sendErrorResponse(res,500,error.message)
-    }
-}
-
-exports.deletePoll = async (req,res) => {
-    try {
-        const poll = await Poll.findByIdAndDelete(req.params.id);
-        if(!poll){
-            return sendErrorResponse(res,404,"Poll not found");
-        }
-        res.status(200).json({
-            success:true,
-            message:"Poll deleted successfully"
-        })
-    } catch (error) {
-        sendErrorResponse(res,500,error.message)
-    }
-}
-
-exports.likeDislikePoll = async (req,res) => {
-    try {
-        const userId=req.user._id;
-        const pollId=req.params.id;
-        const poll = await Poll.findById(pollId)
-        if(!poll){
-            return sendErrorResponse(res,404,"Post not found");
-        }
-        if(poll.likes.includes(userId)){
-            let index=poll.likes.indexOf(userId)
-            poll.likes.splice(index,1);
-            await poll.save()
-            res.status(200).json({
-                success:true,
-                message:"DisLiked Post"
-            })
-        }
-        else{
-            poll.likes.unshift(userId);
-            await poll.save()
-            res.status(200).json({
-                success:true,
-                message:"Liked Post"
-            })
-        }
-        
-    } catch (error) {
-     sendErrorResponse(res,500,error.message)   
-    }
-}
-
-exports.commentOnPoll = async (req,res) => {
-    try {
-        const pollId=req.params.id;
-        const userId=req.user._id;
-        const {comment}=req.body;
-        const poll = await Poll.findById(pollId);
-        if(!poll){
-            return sendErrorResponse(res,404,"Poll not found")
-        }
-        let commentIndex=-1;
-        poll.comments.forEach((item,index)=>{
-            if(item.user.toString()===userId.toString()){
-                commentIndex=index;
-            }
-        })
-
-        if(commentIndex !==-1){
-            //update comment
-            poll.comments[commentIndex].comment=comment  
-            await poll.save();
-            res.status(200).json({
-                success:true,
-                message:"Comment updated"
-            })
-        }
-        else{
-            //add new comment
-            poll.comments.unshift({
-                user:userId,
-                comment
-            })
-            await poll.save();
-            res.status(200).json({
-                success:true,
-                message:"Comment added"
-            })
-        }
-    } catch (error) {
-        sendErrorResponse(res,500,error.message)
-    }
-}
-
-exports.getAllPolls = async (req, res) => {
-    const perPage = 5;
-    try {
-        const page = req.query.page || 1;
-        const polls = await Poll.find()
-            .populate('author', ['name', 'email'])
-            .sort({ createdAt: -1 })
-            .skip((page - 1) * perPage)
-            .limit(perPage);
-
+        const poll = await Poll.create({ title, description, options })
         res.status(200).json({
             success: true,
-            polls
+            message: "Poll created Successfully",
+            poll
         })
     } catch (error) {
         return sendErrorResponse(res, 500, error.message)
     }
 }
 
-exports.getPoll = async(req,res)=>{
+exports.updatePoll = async (req, res) => {
+    try {
+        const fields = ["title", "description", "options", "startDate", "endDate", "tags"]
+        const update = {}
+        fields.forEach((field) => {
+            if (req.body[field] !== undefined) {
+                if (field === 'startDate' || field === 'endDate') {
+                    update[field] = new Date(req.body.field)
+                }
+                else {
+                    update[field] = req.body[field]
+                }
+            }
+        })
+        const poll = await Poll.findByIdAndUpdate(req.params.id, update, { new: true })
+        if (!poll) {
+            return sendErrorResponse(res, 404, "Poll not found")
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Poll updated successfully",
+            poll
+        })
+    } catch (error) {
+        sendErrorResponse(res, 500, error.message)
+    }
+}
+
+exports.deletePoll = async (req, res) => {
+    try {
+        const poll = await Poll.findByIdAndDelete(req.params.id);
+        if (!poll) {
+            return sendErrorResponse(res, 404, "Poll not found");
+        }
+        res.status(200).json({
+            success: true,
+            message: "Poll deleted successfully"
+        })
+    } catch (error) {
+        sendErrorResponse(res, 500, error.message)
+    }
+}
+
+exports.likeDislikePoll = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const pollId = req.params.id;
+        const poll = await Poll.findById(pollId)
+        if (!poll) {
+            return sendErrorResponse(res, 404, "Post not found");
+        }
+        if (poll.likes.includes(userId)) {
+            let index = poll.likes.indexOf(userId)
+            poll.likes.splice(index, 1);
+            await poll.save()
+            res.status(200).json({
+                success: true,
+                message: "DisLiked Post"
+            })
+        }
+        else {
+            poll.likes.unshift(userId);
+            await poll.save()
+            res.status(200).json({
+                success: true,
+                message: "Liked Post"
+            })
+        }
+
+    } catch (error) {
+        sendErrorResponse(res, 500, error.message)
+    }
+}
+
+exports.commentOnPoll = async (req, res) => {
+    try {
+        const pollId = req.params.id;
+        const userId = req.user._id;
+        const { comment } = req.body;
+        const poll = await Poll.findById(pollId);
+        if (!poll) {
+            return sendErrorResponse(res, 404, "Poll not found")
+        }
+        let commentIndex = -1;
+        poll.comments.forEach((item, index) => {
+            if (item.user.toString() === userId.toString()) {
+                commentIndex = index;
+            }
+        })
+
+        if (commentIndex !== -1) {
+            //update comment
+            poll.comments[commentIndex].comment = comment
+            await poll.save();
+            res.status(200).json({
+                success: true,
+                message: "Comment updated"
+            })
+        }
+        else {
+            //add new comment
+            poll.comments.unshift({
+                user: userId,
+                comment
+            })
+            await poll.save();
+            res.status(200).json({
+                success: true,
+                message: "Comment added"
+            })
+        }
+    } catch (error) {
+        sendErrorResponse(res, 500, error.message)
+    }
+}
+
+exports.getAllPolls = async (req, res) => {
+    const perPage = 5;
+    // console.log(req.query);
+    try {
+        const page = req.query.page || 1;
+
+        // Search
+        const searchQuery = req.query.search;
+        const searchFilter = searchQuery
+            ? {
+                $or: [
+                    { title: { $regex: searchQuery, $options: 'i' } },
+                    { description: { $regex: searchQuery, $options: 'i' } },
+                ],
+            }
+            : {};
+
+        // Sorting
+        const sortOption = req.query.sort || 'newer';
+        let sortCriteria = {};
+        if (sortOption === 'older') {
+            sortCriteria = { createdAt: 1 };
+        } else if (sortOption === 'newer') {
+            sortCriteria = { createdAt: -1 };
+        } else if (sortOption === 'likes') {
+            sortCriteria = { likes: -1 };
+        } else if (sortOption === 'votes') {
+            sortCriteria = { totalVotes: -1 };
+        }
+
+        const polls = await Poll.aggregate([
+            {
+                $match: searchFilter,
+            },
+            {
+                $addFields: {
+                    totalVotes: { $sum: '$options.votes' },
+                },
+            },
+            // {
+            //     $lookup: {
+            //         from: 'User',
+            //         localField: 'author',
+            //         foreignField: '_id',
+            //         as: 'authorInfo',
+            //     },
+            // },
+            // {
+            //     $unwind: '$authorInfo',
+            // },
+            {
+                $sort: sortCriteria,
+            },
+            {
+                $skip: (page - 1) * perPage,
+            },
+            {
+                $limit: perPage,
+            },
+        ]);
+
+        res.status(200).json({
+            success: true,
+            polls,
+        });
+        console.log({ polls, page, searchQuery, searchFilter, sortOption });
+    } catch (error) {
+        return sendErrorResponse(res, 500, error.message);
+    }
+};
+
+exports.getPoll = async (req, res) => {
     try {
         const poll = await Poll.findById(req.params.id);
         if (!poll) return sendErrorResponse(res, 404, "Poll not found");
         res.status(200).json({
-          success: true,
-          poll,
+            success: true,
+            poll,
         });
-      } catch (error) {
+    } catch (error) {
         sendErrorResponse(res, 500, error.message);
-      }
+    }
 }
--- client/src/pages/MyFeeds/index.jsx
+++ client/src/pages/MyFeeds/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import VoteItem from '../../components/VoteItem'
 import { Spinner } from '../../components/Loader/SpinLoader';
+import styles from '../../styles';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -10,13 +11,14 @@ const MyFeeds = () => {
     const [page, setPage] = useState(1);
     const [hasMore, setHasMore] = useState(true)
     const [loading, setLoading] = useState(true)
-
+    const [search, setSearch] = useState('');
+    const [sort, setSort] = useState('newer');
     const getPolls = async () => {
         try {
-            const res = await fetch(`${apiUrl}/poll//allPolls?page=${page}`);
+            const res = await fetch(`${apiUrl}/poll/allPolls?page=${encodeURIComponent(page)}&search=${encodeURIComponent(search)}&sort=${sort}`);
             const data = await res.json();
             if (data.polls.length < perPage) setHasMore(false);
-            setFeeds(prevItems => [...prevItems, ...data.polls]);
+            setFeeds(prevItems => [...new Set([...prevItems, ...data.polls])]);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -50,20 +52,58 @@ const MyFeeds = () => {
     }, []);
 
     return (
-        <div className='flex flex-col'>
-            {
-                feeds && feeds.map((feed, index) => (
-                    <div className="bg-white rounded-[10px] overflow-hidden mb-4 px-2 py-1 md:px-6 md:py-2" key={index}>
-                        <VoteItem pollData={feed} />
-                    </div>
-                ))
-            }
-            {
-                (loading && hasMore) && <div className='flex justify-center items-center'><Spinner /></div>
-            }
-            {
-                !hasMore && <h1 className='text-center text-base lg:text-xl font-semibold'>You have Reached to end</h1>
-            }
+        <div className='flex lg:flex-row flex-col-reverse'>
+            <div className='flex flex-col flex-1'>
+                {
+                    feeds.length > 0 ? feeds.map((feed, index) => (
+                        <div className="bg-white rounded-[10px] overflow-hidden mb-4 px-2 py-1 md:px-6 md:py-2" key={index}>
+                            <VoteItem pollData={feed} />
+                        </div>
+                    )) : <div>{!loading && <h1 className='text-center text-base lg:text-xl font-semibold'>No Feeds Found</h1>}</div>
+                }
+                {
+                    // feeds.length === 0 && <h1 className='text-center text-base lg:text-xl font-semibold'>No Feeds Found</h1>
+                }
+                {
+                    (loading && hasMore) && <div className='flex justify-center items-center'><Spinner /></div>
+                }
+                {
+                    (!hasMore && feeds.length > 0) && <h1 className='text-center text-base lg:text-xl font-semibold'>You have Reached to end</h1>
+                }
+            </div>
+            <div className={`lg:ml-7 lg:pl-7 lg:border-l lg:border-slate-400 flex lg:flex-col justify-between lg:justify-start mb-5 lg:mb-0 flex-wrap gap-3 lg:w-[250px] ${styles.heading6}`}>
+                {/* Input for search */}
+                <input
+                    type="text"
+                    placeholder="Search Poll"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className='border-2 border-slate-400 px-3 sm:px-5 lg:py-1 py-0 outline-none focus:border-slate-600 rounded-3xl flex-1 lg:flex-none'
+                />
+
+                {/* Dropdown for sorting */}
+                <select
+                    value={sort}
+                    onChange={(e) => setSort(e.target.value)}
+                    className={`lg:mt-4 ${styles.heading6} font-bold px-3 py-2 rounded-3xl bg-slate-400`}
+                >
+                    <option value="newer">Newer</option>
+                    <option value="older">Older</option>
+                    <option value="likes">Likes</option>
+                    <option value="votes">Votes</option>
+                </select>
+
+                {/* Button to trigger API call */}
+                <button onClick={() => {
+                    setFeeds([]);
+                    setPage(1);
+                    setHasMore(true)
+                    setLoading(true)
+                    setTimeout(() => {
+                        getPolls()
+                    }, 30);
+                }} className={`px-2 md:px-3 py-1 rounded-md  text-black hover:text-white bg-slate-300 hover:bg-sky-400 duration-500 transition-colors font-semibold lg:mt-5`}>Get Polls</button>
+            </div>
         </div>
     )
 }
