700b138515b23a2c3a50a24821a7f9766af7ed13
Abhinav Kumar <abhinavkumar93043@example.com>
2024-09-29 15:05:49 +0530

    frontend env variable

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ app.use(express.json()); //middleware to understand json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 const cors = require("cors");
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 
 // //importing routes
 const user = require("./Router/userRoutes");
