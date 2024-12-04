da6a982988a38e3d205e93fd4223994c81e00bf9
Abhinav Kumar <abhinavkumar93043@example.com>
2024-12-04 12:29:18 +0530

    multiple frontend origin and handle undefined route

--- api/app.js
+++ api/app.js
@@ -3,15 +3,27 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const app = express();
-const path = require('path');
 const ErrorHandling = require("./middlewares/error");
-const mongoose = require('mongoose')
 dotenv.config({ path: "config/config.env" });
 app.use(express.json()); //middleware to understand json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 const cors = require("cors");
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+const allowedOrigins = process.env.FRONTEND_URLS.split(',');
+
+// Configure CORS to allow requests from the allowed origins
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
+}));
 
 // //importing routes
 const user = require("./Router/userRoutes");
@@ -28,14 +40,17 @@ app.use("/api/v1/vote", vote);
 app.use("/api/v1/comment", comment);
 app.use("/api/v1/followers_followings", followerFollowing);
 app.use("/api/v1/save-poll", savePoll);
-app.use('/api/v1/profile-image', express.static(__dirname + '/uploads/profile_image')); // route to  serve the static file(profile image in this project)
-// app.use(express.static(path.join(__dirname, '/dist')));
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Echoes-Insights API!');
+});
 
-// // Catch-all route to serve the React app
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/dist/index.html'));
-// });
+// Handle undefined routes
+app.use((req, res, next) => {
+    const error = new Error(`Cannot find Resource on this server!`);
+    error.statusCode = 404;
+    next(error);
+});
 
 //using error middlewares
 app.use(ErrorHandling);
